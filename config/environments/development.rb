b9edf8d22ca0ddd1dc1af836b304145067007162
Joe Guerra <merlin2049er@gmail.com>
2022-02-25 11:20:12 -0500

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,5 +75,17 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address:               "smtp.gmail.com",
+    port:                  587,
+    user_name:            "JGuerra@jginfosys.com",
+    password:             "",
+    authentication:       "plain",
+    enable_starttls_auto: true
+  }
+
 
 end
